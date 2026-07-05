77e963742de9a72a685cb7d78a0ac52e79aca01a
langloal <langloal@oregonstate.edu>
2026-07-05 17:00:08 -0400

    wip

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,23 +1,42 @@
 // React router + global layout wrapper
 import { useState, useEffect } from 'react'
+import { BrowserRouter as Router, Routes, Route, Navigate, Outlet} from 'react-router-dom';
 import { LoginPage } from './pages/LoginPage'
+import NavigationBar  from './components/NavigationBar';
 import ServerHealth from "./components/ServerHealth";
+
+function AuthenticatedLayout() {
+  return (
+    <div className="App">
+      <center>
+        <NavigationBar />
+        <Outlet />
+      </center>
+    </div>
+  );
+}
 
 
 function App() {
-  const [count, setCount] = useState(0);
+   const isLoggedIn = false;
 
   return (
-    <>
-    {/* Component: ServerHealth */}
-    <ServerHealth />
+    <Router>
+      {/* Component: ServerHealth */}
+      <ServerHealth />
+      <Routes>
+          <Route path="/login" element={<LoginPage />} />
 
-    <div className="App">
-      <center>
-        <LoginPage/>
-      </center>
-    </div>
-    </>
+          <Route element={isLoggedIn ? <AuthenticatedLayout /> : <Navigate to="/login" />}>
+            {/* Navigation routes seen on Homepage*/}
+            <Route path="/" element={<div>Welcome to the Homepage!</div>} />
+            <Route path="/search-experiences" element={<div>Search Page Content</div>} />
+            <Route path="/user-profile" element={<div>Profile Page Content</div>} />
+          </Route>
+
+          <Route path="*" element={<Navigate to={isLoggedIn ? "/" : "/login"} />} />
+      </Routes>
+    </Router>
   )
 }
 
