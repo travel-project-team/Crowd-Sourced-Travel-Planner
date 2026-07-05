942db83266bb3dff0ef324df9ee65d4870af9d11
langloal <langloal@oregonstate.edu>
2026-07-05 11:26:26 -0400

    finished style layout

--- frontend/src/pages/LoginPage.jsx
+++ frontend/src/pages/LoginPage.jsx
@@ -1,24 +1,27 @@
 // Citation: https://www.youtube.com/watch?v=Y-XW9m8qOis
 
 import React, {useState} from "react";
+import '../styles/LoginPage.css';
 
 export const LoginPage = () => {
     const [email, setEmail] = useState('');
     const [pass, setPass] = useState('');
 
-    const handleSubmit = () => {
+    const handleSubmit = (e) => {
         e.preventDefault();
         console.log(email);
     }
 
     return (
-        <form>
+        <div className="auth-form-container">
+            <form className="login-form" onSubmit={handleSubmit}>
             <label for="email">email</label>
             <input value={email} type = "email" placeholder="youremail@beaver.com" id="email" name="email"></input>
             <label for="password">password</label>
             <input value={pass} type = "password" placeholder="********" id="password" name="password"></input>
-            <button>Login</button>
-        </form>
+            <button type="submit">Login</button>
+            </form>
+        </div>
     );
 
 };
