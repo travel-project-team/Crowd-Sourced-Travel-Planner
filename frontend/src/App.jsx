b1948aa28964361faae673e229a681c6d36b8c0e
langloal <langloal@oregonstate.edu>
2026-07-08 21:59:23 -0400

    user can toggle between login and registration forms

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,13 @@
-// React router + global layout wrapper
+// Citation: https://youtu.be/137uPoV_3xE?si=vYzo8cboZbOsgogC
+
+// React libraries
 import { useState, useEffect } from 'react'
 import { BrowserRouter as Router, Routes, Route, Navigate, Outlet} from 'react-router-dom';
+
+// Styling
+import "./styles/App.css"
+
+// Custom
 import ServerHealth from "./components/common/ServerHealth";
 import { Login } from './components/feature/Login';
 import { Registration } from './components/feature/Registration';
@@ -8,13 +15,23 @@ import { DashboardLayout } from './layouts/DashboardLayout';
 
 
 function App() {
-   const isLoggedIn = true;
+   const [showLogin, setShowLogin] = useState(false);
 
   return (
     <>
       <ServerHealth />
-      <Login/>
-      {/* <Registration/> */}
+      <div>
+        {showLogin ? <Login /> : <Registration />}
+        <div className='toggle-wrapper'>
+          {showLogin ? (
+            <p>Don't have an account? { " "}
+            <button className="toggle-btn" onClick={() => setShowLogin(false)}>Register</button></p>
+          ):(
+            <p>Already have an account? { " "}
+            <button className="toggle-btn" onClick={() => setShowLogin(true)}>Login</button></p>
+          )}
+        </div>
+      </div>
     </>
   )
 }
