835e0a743a1a7ec426283a7a1eca911e6f89be45
Kevin Lin <kevin.sam.lin@gmail.com>
2026-07-19 19:50:23 -0500

    Initialize Google Cloud

--- backend/src/main.py
+++ backend/src/main.py
@@ -2,7 +2,8 @@
 
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-import uvicorn
+from fastapi.staticfiles import StaticFiles
+import uvicorn, os
 from contextlib import asynccontextmanager
 
 from src.config import initialize_services, close_services
@@ -20,8 +21,6 @@ async def lifespan(app: FastAPI):
 
 app = FastAPI(lifespan=lifespan)
 
-# CORS configuration --Currently bypassed by Vite Proxy in development 
-# Keep for production deployment
 origins = [
     "http://localhost:9000",
     "http://127.0.0.1:9000",
@@ -40,6 +39,10 @@ app.include_router(trips_router)
 app.include_router(users_router)
 app.include_router(experiences_router)
 
+# Production - Handles frontend as static file
+if os.path.exists("static"):
+    app.mount("/", StaticFiles(directory="static", html=True), name="static")
+
+# Development - Hot reloading server
 if __name__ == "__main__":
-    # Enable hot reloading development server
     uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
