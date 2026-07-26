439a61251cc47ec996d1908ce3e3f54200b757a8
Kevin Lin <kevin.sam.lin@gmail.com>
2026-07-25 19:49:29 -0500

    Add user experience endpoint and clean up assets

--- frontend/src/pages/ExperiencePage.jsx
+++
@@ -1,9 +0,0 @@
-import DummyData from "../data/DummyData";
-
-export default function ExperiencePage() {
-    return (
-        <div>
-            <h1>Experience Page</h1>
-        </div>
-    );
-}
--- frontend/src/pages/TripsPage.jsx
+++
@@ -1,9 +0,0 @@
-import DummyData from "../data/DummyData";
-
-export default function TripsPage() {
-    return (
-        <div>
-            <h1>Trips Page</h1>
-        </div>
-    );
-}
